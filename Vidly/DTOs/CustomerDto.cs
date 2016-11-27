39f2e940a542494e67d94bf816565e09d0e6f147
githubDebanjan16 <debanjan.pradhan@gmail.com>
2016-11-27 02:33:03 -0600

    NOV:Section6 WebAPI

---
+++ Vidly/DTOs/CustomerDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        //[Min18YrsIfAMember]
+        [Display(Name = "Date of Birth")]
+        public DateTime? BirthDate { get; set; }
+    }
+}
---
+++ Vidly/DTOs/MovieDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime DateReleased { get; set; }
+
+        [Required]
+        [Range(1, 30)]
+        public byte NumberInStock { get; set; }
+    }
+}
